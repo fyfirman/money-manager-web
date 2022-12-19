e75f3270c22851f19e1438a8d32ee7a5a1d8aa6f
Firmansyah Yanuar <firmansyah.yanuar@autobahn-security.com>
2022-12-19 21:33:25 +0700

    chore: disable client side

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      // Rewrite everything to `pages/index`
-      {
-        source: "/:any*",
-        destination: "/",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
