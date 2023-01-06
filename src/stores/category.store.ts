cbfe37d0ee4e9dc7cb1d5f10bda627772a8cf2c3
Firmansyah Yanuar <fyfirman@gmail.com>
2023-01-06 23:49:13 +0700

    feat: get category & sub category by name function

--- src/stores/category.store.ts
+++ src/stores/category.store.ts
@@ -1,5 +1,6 @@
 import create from "zustand";
 import { devtools } from "zustand/middleware";
+import { GetElementType } from "~/interfaces/helper";
 
 export enum CategoryType {
   Expense = "expense",
@@ -13,10 +14,14 @@ export interface Category {
   children: Omit<Category, "children" | "type">[];
 }
 
+export type SubCategory = GetElementType<Category["children"]>;
+
 export interface CategoryState {
   categories: Category[];
-  getSubCategories(id: Category["id"]): Category["children"];
-  getSubCategoriesByName(name: Category["name"]): Category["children"];
+  getSubCategories(id: Category["id"]): SubCategory[];
+  getSubCategoriesByName(name: Category["name"]): SubCategory[];
+  getCategoryByName(name: Category["name"]): Category | undefined;
+  getSubCategoryByName(name: SubCategory["name"]): SubCategory | undefined;
 }
 
 export const useCategoryStore = create<CategoryState>()(
@@ -25,7 +30,7 @@ export const useCategoryStore = create<CategoryState>()(
       categories: [] as Category[],
       getSubCategories(id) {
         const { categories } = get();
-        const result = categories.find((subCategory) => subCategory.id === id);
+        const result = categories.find((category) => category.id === id);
 
         if (!result) {
           return [];
@@ -35,13 +40,44 @@ export const useCategoryStore = create<CategoryState>()(
       },
       getSubCategoriesByName(name) {
         const { categories } = get();
-        const result = categories.find((subCategory) => subCategory.name === name);
+        const result = categories.find((category) => category.name === name);
 
         if (!result) {
           return [];
         }
 
         return result.children;
+      },
+      getCategoryByName(name) {
+        const { categories } = get();
+        const result = categories.find((category) => category.name === name);
+
+        if (!result) {
+          return undefined;
+        }
+
+        return result;
+      },
+      getSubCategoryByName(name) {
+        const { categories } = get();
+
+        let result: SubCategory | undefined;
+
+        categories.forEach((category) => {
+          if (category.children.length === 0) {
+            return;
+          }
+
+          const temp = category.children.find((subCategory) => subCategory.name === name);
+
+          if (!temp) {
+            return;
+          }
+
+          result = temp;
+        });
+
+        return result;
       },
     }),
     {
