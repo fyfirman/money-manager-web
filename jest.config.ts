8bce4d7975c6ba641e4df9a8f78bab3af0b4e43c
Firmansyah Yanuar <fyfirman@gmail.com>
2023-01-13 01:54:56 +0700

    chore: configure jest

--- jest.config.ts
+++ jest.config.ts
@@ -7,10 +7,13 @@ const config: Config = {
   testEnvironment: "jsdom",
   verbose: true,
   transform: {
-    ".(ts|tsx)": "ts-jest",
+    ".(ts|tsx)": "@swc/jest",
   },
   testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
   moduleFileExtensions: ["ts", "tsx", "js"],
+  moduleNameMapper: {
+    "~/(.*)$": "<rootDir>/src/$1",
+  },
 };
 
 export default config;
